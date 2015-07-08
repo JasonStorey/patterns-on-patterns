4a0bc6f3539d1ddbf527c4a62163ff55ef19f27c
Jason A. Storey <jason.andrew.storey@gmail.com>
2015-07-08 23:32:32 +0100

    Append a placeholder image and buck wild with the ES6 biz.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const imageSources = ['https://placeholdit.imgix.net/~text?txtsize=33&txt=Buddy?&w=320&h=500'];
+
+function createImageElem(src) {
+    let img = document.createElement('img');
+    img.setAttribute('src', src);
+    return img;
+}
+
 module.exports = {
-  init: function() {
-    console.log('does nothing... yet');
+  init: config => {
+      imageSources
+          .map(createImageElem)
+          .forEach(elem => config.container.appendChild(elem));
   }
 };
