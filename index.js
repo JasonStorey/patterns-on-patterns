b10d1e8f16be3f71ff9a9c49632f4e5b615db1ff
Jason A. Storey <jason.andrew.storey@gmail.com>
2015-07-08 23:59:42 +0100

    CSS reset

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const imageSources = ['https://placeholdit.imgix.net/~text?txtsize=33&txt=Buddy?&w=320&h=500'];
+const imageSources = [
+    'https://placeholdit.imgix.net/~text?txtsize=33&txt=Buddy,&w=320&h=300',
+    'https://placeholdit.imgix.net/~text?txtsize=33&txt=don\'t&w=320&h=300',
+    'https://placeholdit.imgix.net/~text?txtsize=33&txt=buddy&w=320&h=300',
+    'https://placeholdit.imgix.net/~text?txtsize=33&txt=me,&w=320&h=300',
+    'https://placeholdit.imgix.net/~text?txtsize=33&txt=Buddy.&w=320&h=300'
+];
 
 function createImageElem(src) {
     let img = document.createElement('img');
