48b567cc6ab9cdb9bbdab6ab809cc66d8d8f3020
Jason A. Storey <jason.andrew.storey@gmail.com>
2015-07-23 17:09:07 +0100

    Switch to using background image tween with basic physics for animation

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-function createImageElem(imageConfig) {
-    let img = document.createElement('img');
-    img.setAttribute('src', imageConfig.url);
-    return img;
-}
-
-function setupLoopScrolling(document, patternsConfig) {
-    $(document).scroll(() => {
-        if($(document).scrollTop() >= patternsConfig.height) {
-            $(document).scrollTop(0);
-        }
-    });
-    return patternsConfig;
-}
-
 function getPatternsConfig() {
     return new Promise((res, rej) => {
         $.getJSON('./patterns-config.json')
@@ -21,14 +6,47 @@ function getPatternsConfig() {
     });
 }
 
+function setupMouseMovement(mainConfig) {
+    var $container = $(mainConfig.container),
+        midPoint = $container.height() / 2,
+        velocityY = 0,
+        currentY = 0,
+        drag = 0.4;
+
+    $(mainConfig.document).on('mousemove', (event) => {
+        velocityY = getYVelocity(velocityY, event.clientY, midPoint) * (1 - drag);
+    });
+
+    function updateBackgroundPosition() {
+        currentY += velocityY;
+        $container.css('background-position', 'center ' + currentY + 'px');
+        setTimeout(updateBackgroundPosition, 16);
+    }
+
+    updateBackgroundPosition();
+}
+
+function getYVelocity(velocityY, mouseY, midY) {
+    var deltaY = mouseY - midY;
+    return velocityY + (deltaY / 10);
+}
+
 module.exports = {
-  init: config => {
+  init: mainConfig => {
 
       getPatternsConfig()
-          .then(patternsConfig => setupLoopScrolling(config.document, patternsConfig))
-          .then(patternsConfig => patternsConfig.images.concat(patternsConfig.images))
-          .then(imagesConfig => imagesConfig.map(createImageElem))
-          .then(images => images.forEach(elem => config.container.appendChild(elem)))
+          .then(patternsConfig => {
+              mainConfig.patternsConfig = patternsConfig;
+
+              $(mainConfig.container).css({
+                  'background-image': 'url(' + patternsConfig.images[0].url + ')',
+                  'background-repeat': 'repeat-y',
+                  'background-position': 'centre'
+              });
+
+              return mainConfig;
+          })
+          .then(mainConfig => setupMouseMovement(mainConfig))
           .catch(err => {
               console.log(err);
           });
